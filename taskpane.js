0e7232745316455c27e51d2be6247209f5825cc7
Javargo <58937411+Javargo@users.noreply.github.com>
2021-03-02 15:31:24 +0100

    Update taskpane.js

--- taskpane.js
+++ taskpane.js
@@ -1,35 +1,13 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
-// images references in the manifest
-import "../../assets/icon-16.png";
-import "../../assets/icon-32.png";
-import "../../assets/icon-80.png";
-
-/* global document, Office, Word */
-
 Office.onReady(info => {
-  if (info.host === Office.HostType.Word) {
-    document.getElementById("sideload-msg").style.display = "none";
-    document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
-  }
+	document.getElementById('begu').textContent='Taskpanel loaded in '+info.host;
+	if (info.host === Office.HostType.Word) {
+	}
 });
 
-export async function run() {
-  return Word.run(async context => {
-    /**
-     * Insert your Word code here
-     */
-
-    // insert a paragraph at the end of the document.
-    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
-
-    // change the paragraph color to blue.
-    paragraph.font.color = "blue";
-
-    await context.sync();
-  });
+async function run() {
+	return Word.run(async context => {
+    	const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+    	paragraph.font.color = "blue";
+    	await context.sync();
+  	});
 }
