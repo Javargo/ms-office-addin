9a752c9837c16a0be5bbd03e8ecd3f6a0ed5b0e6
Javargo <58937411+Javargo@users.noreply.github.com>
2021-06-16 13:40:20 +0200

    Update taskpane.js

--- taskpane.js
+++ taskpane.js
@@ -5,8 +5,10 @@ Office.onReady(info => {
 });
 
 async function run() {
+	Office.context.document.customXmlParts.addAsync(
+        '<root categoryId="1" xmlns="http://tempuri.org"><item name="Cheap Item" price="$193.95"/><item name="Expensive Item" price="$931.88"/></root>',
+        function (result) {});
 	return Word.run(async context => {
-	//const z=context.document.customXmlParts.getCount();
     	const paragraph = context.document.body.insertParagraph("Hello again!", Word.InsertLocation.end);
     	paragraph.font.color = "blue";
     	await context.sync();
